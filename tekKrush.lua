73ba7731d61f2f346824930a8d1c7b51e0d7a514
Kristi <kristi.dev@gmail.com>
2012-04-14 10:35:09 -0700

    Confirm bind too

--- tekKrush.lua
+++ tekKrush.lua
@@ -1,10 +1,27 @@
 local f = CreateFrame("Frame")
 f:RegisterEvent("CONFIRM_DISENCHANT_ROLL")
+f:RegisterEvent("CONFIRM_LOOT_ROLL")
+f:RegisterEvent("LOOT_BIND_CONFIRM")
 f:SetScript("OnEvent", function(self, event, id, rollType)
-	for i=1,STATICPOPUP_NUMDIALOGS do
-		local frame = _G["StaticPopup"..i]
-		if frame.which == "CONFIRM_LOOT_ROLL" and frame.data == id and frame.data2 == rollType and frame:IsVisible() then StaticPopup_OnClick(frame, 1) end
-	end
+-- TekKrush based code from ShestakUI
+        if GetNumRaidMembers() == 0 and GetNumPartyMembers() == 0 then
+                local elapsed = 0
+                self:SetScript("OnUpdate", function(self, elap)
+                        elapsed = elapsed + elap
+                        if elapsed < 0.2 then
+                                StaticPopup_Hide("LOOT_BIND")
+                                return
+                        end
+                        elapsed = 0
+                        ConfirmLootSlot(id)
+                        self:SetScript("OnUpdate", nil)
+                end)
+        else
+                for i = 1, STATICPOPUP_NUMDIALOGS do
+                        local frame = _G["StaticPopup"..i]
+                        if (frame.which == "CONFIRM_LOOT_ROLL" or frame.which == "LOOT_BIND" or frame.which == "LOOT_BIND_CONFIRM") and frame:IsVisible() then StaticPopup_OnClick(frame, 1) end
+                end
+        end
 end)
 
 
